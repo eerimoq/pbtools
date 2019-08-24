0832dd248e137e55fc3868b61bfce4c2c3ceb9b3
Erik Moqvist <erik.moqvist@gmail.com>
2019-08-24 11:31:12 +0200

    Benchmark.

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
 test:
+	$(MAKE) test-python
+	$(MAKE) test-c
+
+test-python:
 	python3 setup.py test
 
 test-c:
 	make -C examples/address_book
 	examples/address_book/main
+	make -C examples/benchmarks/c_source
 	make -C tests
 
 release-to-pypi:
---
+++ examples/benchmarks/c_source/pbtools/main.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <string.h>
+
+#include "benchmark.h"
+
+int main(void)
+{
+    struct benchmark_benchmark_t *benchmark_p;
+    uint8_t encoded[256];
+    uint8_t workspace[64];
+
+    benchmark_p = benchmark_benchmark_new(&workspace[0], sizeof(workspace));
+    benchmark_p->v1 = 1000000;
+    assert(benchmark_benchmark_encode(benchmark_p,
+                                      &encoded[0],
+                                      sizeof(encoded)) == 4);
+    assert(memcmp(&encoded[0], "\x08\xc0\x84\x3d", 4) == 0);
+
+    return (0);
+}
